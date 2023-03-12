1e320d00fc8d1bbf386eb4f6d0aa4e3e56586ad9
HUIKIM0 <fehs123@naver.com>
2023-03-12 16:38:19 +0900

    Initial commit

---
+++ HomeControl_Arduino/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace HomeControl_Arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboxSelectPort = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnON = new System.Windows.Forms.Button();
+            this.btnOFF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM 포트설정";
+            // 
+            // cboxSelectPort
+            // 
+            this.cboxSelectPort.FormattingEnabled = true;
+            this.cboxSelectPort.Items.AddRange(new object[] {
+            "포트를 선택"});
+            this.cboxSelectPort.Location = new System.Drawing.Point(15, 51);
+            this.cboxSelectPort.Name = "cboxSelectPort";
+            this.cboxSelectPort.Size = new System.Drawing.Size(103, 23);
+            this.cboxSelectPort.TabIndex = 1;
+            this.cboxSelectPort.Click += new System.EventHandler(this.cboxSelectPort_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(125, 46);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 35);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "연결하기";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnON
+            // 
+            this.btnON.Location = new System.Drawing.Point(15, 97);
+            this.btnON.Name = "btnON";
+            this.btnON.Size = new System.Drawing.Size(103, 32);
+            this.btnON.TabIndex = 7;
+            this.btnON.Text = "켜기";
+            this.btnON.UseVisualStyleBackColor = true;
+            this.btnON.Click += new System.EventHandler(this.btnON_Click);
+            // 
+            // btnOFF
+            // 
+            this.btnOFF.Location = new System.Drawing.Point(124, 97);
+            this.btnOFF.Name = "btnOFF";
+            this.btnOFF.Size = new System.Drawing.Size(101, 32);
+            this.btnOFF.TabIndex = 8;
+            this.btnOFF.Text = "끄기";
+            this.btnOFF.UseVisualStyleBackColor = true;
+            this.btnOFF.Click += new System.EventHandler(this.btnOFF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 159);
+            this.Controls.Add(this.btnOFF);
+            this.Controls.Add(this.btnON);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboxSelectPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboxSelectPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnON;
+        private System.Windows.Forms.Button btnOFF;
+    }
+}
+
---
+++ HomeControl_Arduino/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace HomeControl_Arduino
+{
+    public partial class Form1 : Form
+    {
+        SerialPort mySerial = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboxSelectPort.Text = "포트선택";
+        }
+
+        private void cboxSelectPort_Click(object sender, EventArgs e)
+        {
+            cboxSelectPort.Items.Clear();
+            foreach (var item in SerialPort.GetPortNames())
+            {
+                cboxSelectPort.Items.Add(item);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (cboxSelectPort.Text == "") return;
+            try
+            {
+                if (mySerial.IsOpen)
+                {
+                    mySerial.Close();
+                }
+                else
+                {
+                    mySerial.PortName = cboxSelectPort.SelectedIndex.ToString();
+                    mySerial.BaudRate = 9600;
+                    mySerial.DataBits = 8;
+                    mySerial.StopBits = StopBits.One;
+                    mySerial.Parity = Parity.None;
+                    mySerial.Open();
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("연결에러", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            btnConnect.Text = mySerial.IsOpen ? "연결해제" : "연결하기";
+            cboxSelectPort.Enabled = !mySerial.IsOpen;
+        }
+
+        private void btnON_Click(object sender, EventArgs e)
+        {
+            if (mySerial.IsOpen) return;
+            mySerial.Write("1");
+        }
+
+        private void btnOFF_Click(object sender, EventArgs e)
+        {
+            if (mySerial.IsOpen) return;
+            mySerial.Write("0");
+        }
+    }
+}
